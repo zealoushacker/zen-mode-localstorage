54e51fcdc5115820a699ca447fd9050e0dc7c61a
Alex Notov <anotov@gmail.com>
2013-11-21 21:03:25 -0800

    Button for swapping zen mode

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 $(function() {
-  // zen mode shall be defined as:
-  // hide all elements inside <body>, except article
-  $("body > :not(article)").hide();
+  $(".zen-mode").click(function() {
+    // zen mode shall be defined as:
+    // hide all elements inside <body>, except article
+    $("body > :not(article)").toggle();
+
+    // toggle the icon displayed for the zen mode button
+    $("button.zen-mode > span.glyphicon").toggleClass("glyphicon-resize-small glyphicon-fullscreen");
+
+    // swap the text of the button
+    var toggleButtonText = $("button.zen-mode > span.title").text();
+    $("button.zen-mode > span.title").text(toggleButtonText == "Enable Zen Mode " ? "Disable Zen Mode " : "Enable Zen Mode ");
+  });
 });
